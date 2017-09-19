2be45fe04defb54e7a60c5d879ebafaf587209a9
StMrKirk <disaseractiv@gmail.com>
2017-09-19 12:14:07 +0300

    Make code easier

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,6 +1,6 @@
 import re
 import sys
-import operator
+from collections import Counter
 
 
 def load_data(filepath):
@@ -9,25 +9,17 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text_string):
-    frequency_dict = {}
-    match_pattern = re.findall(r'(\b[a-z]+\b|\b[а-я]+\b)', text_string)
-    for word in match_pattern:
-        count = frequency_dict.get(word, 0)
-        frequency_dict[word] = count + 1
-    return frequency_dict
-
-
-def sort_frequency(frequency_dict):
-    frequency_list_sorted = sorted(frequency_dict.items(), key=operator.itemgetter(1), reverse=True)
-    return frequency_list_sorted
+    match_pattern = re.findall(r'\w+', text_string)
+    return Counter(match_pattern).most_common()
 
 
 def pprint_frequency(frequency_list):
     for word in frequency_list:
         print(word[0], '-', word[1])
 
+
 if __name__ == '__main__':
     text = load_data(sys.argv[1])
     frequency_dict = get_most_frequent_words(text)
-    frequency_list = sort_frequency(frequency_dict)
-    pprint_frequency(frequency_list)
+    pprint_frequency(frequency_dict)
+
